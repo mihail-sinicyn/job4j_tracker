f96857be240bfe951a582259d21afb2d79204371
mihail-sinicyn <sinicyn.mihail@gmail.com>
2021-05-20 18:18:41 +0300

    1.7. Конструктор [#503762]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,34 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    private boolean active;
+
+    private int status;
+
+    private String message;
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public Error() {
+    }
+
+    public void printError() {
+        System.out.println("active: " + active);
+        System.out.println("status:  " + status);
+        System.out.println("message: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error error404 = new Error(true, 404, "Not Found");
+        error404.printError();
+        Error error409 = new Error(true, 409, "Conflict");
+        error409.printError();
+        Error error = new Error();
+        error.printError();
+    }
+}
