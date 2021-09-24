18b9a243f84674fbbaa6722819fe24d5279eb245
mihail-sinicyn <sinicyn.mihail@gmail.com>
2021-09-24 23:33:51 +0300

    3. Интерфейс Input [#181092 #207630]

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -4,10 +4,12 @@ public class Bus implements Transport {
 
     @Override
     public void ride() {
+        System.out.println("Поехали");
     }
 
     @Override
     public void passengers(int sum) {
+        System.out.println("Количество пасажиров: " + sum);
     }
 
     @Override
