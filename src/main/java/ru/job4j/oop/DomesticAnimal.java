cecda197086963577a45b5d77abfc2602b1d62c6
mihail-sinicyn <sinicyn.mihail@gmail.com>
2021-09-26 01:03:04 +0300

    3.2. Зачем нужно приведение типов [#310019] животные

---
+++ src/main/java/ru/job4j/oop/DomesticAnimal.java
@@ -0,0 +1,8 @@
+package ru.job4j.oop;
+/*
+public class DomesticAnimal extends Animal {
+    public void liveOnFarm() {
+        System.out.println(nameClass + " живет на ферме, приносит пользу.");
+    }
+}
+*/
---
+++ src/main/java/ru/job4j/oop/Pet.java
@@ -0,0 +1,8 @@
+package ru.job4j.oop;
+/*
+public class Pet extends Animal {
+    public void vaccinate() {
+        System.out.println(nameClass + " нуждается в прививках, чтобы не болеть.");
+    }
+}
+*/
