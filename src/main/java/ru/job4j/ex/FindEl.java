06422d2ef4b842008bd96d9d6e2f489c70ff6604
mihail-sinicyn <sinicyn.mihail@gmail.com>
2021-10-10 22:49:06 +0300

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,25 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].equals(key)) {
+                rsl = i;
+            }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("Искомый элемент не найден");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        String[] array = {"111", "xyz", "example"};
+        try {
+            System.out.println(indexOf(array, "test"));
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
