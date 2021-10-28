6b9e2fd4a2f722c9125f375bb0e7a7a7a8dccd2d
mihail-sinicyn <sinicyn.mihail@gmail.com>
2021-10-29 00:52:12 +0300

    2. final [#334073]

---
+++ src/main/java/ru/job4j/ooa/Car.java
@@ -0,0 +1,7 @@
+package ru.job4j.ooa;
+
+public final class Car {
+    public void printMark() {
+        System.out.println("Метод выводит марку автомобиля");
+    }
+}
---
+++ src/main/java/ru/job4j/ooa/FinalUsage.java
@@ -0,0 +1,9 @@
+package ru.job4j.ooa;
+
+public class FinalUsage {
+    public static void main(String[] args) {
+        final String NAME = "Petr";
+
+        System.out.println(NAME);
+    }
+}
