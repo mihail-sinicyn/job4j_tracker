fc1d1b44d3689cadb705eafd0c14bf3e241313cf
mihail-sinicyn <sinicyn.mihail@gmail.com>
2021-11-18 22:41:40 +0300

    0. Коллекции, ArrayList, List, Обобщения [#10094 #230250]

--- src/main/java/ru/job4j/collection/UsageArrayList.java
+++ src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class UsageArrayList {
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         ArrayList<String> names = new ArrayList<>();
         names.add("Petr");
         names.add("Ivan");
