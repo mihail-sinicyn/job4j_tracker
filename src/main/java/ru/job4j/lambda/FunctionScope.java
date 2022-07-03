5998f9caec561a19359e8514dbe875a51f288932
mihail-sinicyn <sinicyn.mihail@gmail.com>
2022-07-03 03:37:52 +0300

    5. Подсчет функции в диапазоне. [#24251]

---
+++ src/main/java/ru/job4j/lambda/FunctionScope.java
@@ -0,0 +1,16 @@
+package ru.job4j.lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+public class FunctionScope {
+    List<Double> diapason(int start, int end, Function<Double, Double> func) {
+        List<Double> result = new ArrayList<>();
+        for (int index = start; index < end; index++) {
+            result.add(func.apply((double) index));
+        }
+
+        return result;
+    }
+}
---
+++ src/test/java/ru/job4j/lambda/FunctionScopeTest.java
@@ -0,0 +1,36 @@
+package ru.job4j.lambda;
+
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class FunctionScopeTest {
+    @Test
+    public void whenLinearFunctionThenLinearResults() {
+        FunctionScope function = new FunctionScope();
+        List<Double> result = function.diapason(5, 8, x -> 2 * x + 1);
+        List<Double> expected = Arrays.asList(11D, 13D, 15D);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenQuadraticFunctionThenQuadraticResults() {
+        FunctionScope function = new FunctionScope();
+        List<Double> result = function.diapason(3, 6, x -> Math.pow(x, 2) + 2 * x + 1);
+        List<Double> expected = Arrays.asList(16D, 25D, 36D);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenExponentialFunctionThenExponentialResults() {
+        FunctionScope function = new FunctionScope();
+        List<Double> result = function.diapason(2, 6, x -> Math.pow(2, x) - x + 1);
+        List<Double> expected = Arrays.asList(3D, 6D, 13D, 28D);
+        assertThat(result, is(expected));
+    }
+}
