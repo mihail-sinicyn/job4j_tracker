0707a6dd5e484f161206b56195d359cc5089e86a
mihail-sinicyn <sinicyn.mihail@gmail.com>
2022-07-07 03:15:10 +0300

    0.1. Отладка лямбда в IDEA [#259165]

---
+++ src/main/java/ru/job4j/StreamUsage.java
@@ -0,0 +1,31 @@
+package ru.job4j;
+
+import java.util.List;
+
+public class StreamUsage {
+    public static class Task {
+        private final String name;
+        private final long spent;
+
+        public Task(String name, long spent) {
+            this.name = name;
+            this.spent = spent;
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Task> tasks = List.of(
+                new Task("Bug #1", 10),
+                new Task("Task #2", 50),
+                new Task("Bug #3", 40),
+                new Task("Task #4", 31),
+                new Task("Bug #5", 28),
+                new Task("Bug #6", 37)
+        );
+        tasks.stream()
+                .filter(task -> task.name.contains("Bug"))
+                .filter(task -> task.spent > 30)
+                .map(task -> task.name + " " + task.spent)
+                .forEach(System.out::println);
+    }
+}
